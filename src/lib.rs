129ba29a8ebf70f595618f840bcc7ab9ea2f9667
viperscape <chrisfgill@gmail.com>
2015-10-22 23:16:15 -0400

    crates, and working example

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,56 @@
 extern crate crypto;
+extern crate clock_ticks;
+extern crate rand;
 
+use crypto::digest::Digest;
+use crypto::sha2::Sha256;
+use crypto::hmac::Hmac;
+use crypto::mac::{Mac,MacResult};
+
+use clock_ticks::precise_time_ns;
+
+use rand::random;
+
+
+#[derive(Debug)]
 pub struct Client {
     tid: u64, //long term (tombstone) client id, never changes
-    t: f64, // time of initial connection
-    
+    et: u64, // epoch time of initial connection
+    key: Vec<u8>, //shared key
 }
 
+impl Client {
+    pub fn blank() -> Client {
+        let mut k = vec!();
+        for n in (0..64) { k.push(random::<u8>()); }
+        
+        Client {
+            tid: 0,
+            et: 0,
+            key: k,
+        }
+    }
+}
 
 pub struct Msg {
-    mid: u32, //short term, message id, always changes
-    dt: f64, // delta time since initial connection
+    pub mid: Vec<u8>, //short term, message id, always changes
+    pub data: Vec<u8>,
+}
 
-    data: [u8],
+impl Msg {
+    pub fn new (client: &Client, data: &[u8]) -> Msg {
+        let dt = precise_time_ns() - client.et;
+        
+        let mut hmac = Hmac::new(Sha256::new(),&client.key[..]);
+        
+        Msg {
+            data: data.to_vec(),
+            mid: hmac.result().code().to_vec(),
+        }
+    }
+
+    pub fn auth (client: &Client, msg: &Msg) -> bool {
+        let mut hmac = Hmac::new(Sha256::new(),&client.key[..]);
+        &msg.mid[..] == hmac.result().code()
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,53 @@
-//extern crate crypto;
-//use crypto::digest::Digest;
-//use crypto::sha2::Sha256;
+extern crate iota;
+use iota::{Msg,Client};
 
+use std::time::Duration;
+use std::net::{SocketAddrV4, TcpStream, UdpSocket, TcpListener, Ipv4Addr};
 
 fn main() {
-    use std::time::Duration;
-    use std::net::{SocketAddrV4, TcpStream, UdpSocket, TcpListener, Ipv4Addr};
-    
     let ip = Ipv4Addr::new(127, 0, 0, 1);
     let port = 12345;
     
     if let Some(mut socket) = UdpSocket::bind(SocketAddrV4::new(ip, port)).ok() {
         socket.set_read_timeout(Some(Duration::new(1,0)));
-        let mut v = vec!();
-        for n in "hello".bytes() { v.push(n); }
         
-        socket.send_to(&v,(ip,port));
-        let mut buf = [0; 1024];
-        match socket.recv_from(&mut buf) {
-            Ok((amt, src)) => {
-                let buf = &mut buf[..amt];
-                println!("{:?}",(buf,src));
-            },
-            Err(_) => {panic!("whoa")},
+        let client = Client::blank();
+        let msg = Msg::new(&client,&b"Hello".to_vec()[..]);
+        
+        socket.send_to(&msg.mid,(ip,port));
+        socket.send_to(&msg.data,(ip,port));
+        
+        let msg = collect_msg(&mut socket);
+
+        if Msg::auth(&client,&msg) {
+            println!("auth {:?}",msg.data);
         }
+        else { println!("not auth {:?}",msg.mid) }
 
     }
+
+}
+
+fn collect_msg(socket: &mut UdpSocket) -> Msg {
+    let mut buf = [0; 1024];
+    let mut mid: Vec<u8> = vec!();
+    let mut data: Vec<u8> = vec!();
+    
+    match socket.recv_from(&mut buf) {
+        Ok((amt, src)) => {
+            let r = &mut buf[..amt];
+            mid = r.to_vec();
+        },
+        Err(_) => {panic!("whoa")},
+    }
+
+    match socket.recv_from(&mut buf) {
+        Ok((amt, src)) => {
+            let r = &mut buf[..amt];
+            data = r.to_vec();
+        },
+        Err(_) => {panic!("whoa")},
+    }
+
+    Msg { mid: mid, data: data }
 }
