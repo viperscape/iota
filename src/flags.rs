48a0a033427f4423f3b20cc9c6944ad4f3daa7ca
viperscape <chrisfgill@gmail.com>
2015-10-25 15:05:17 -0400

    rework flags

--- src/flags.rs
+++ src/flags.rs
@@ -3,12 +3,14 @@
 bitflags! {
     
     flags Flags: u8 {
-        const Ping = 1,
-        const Req = 1 << 1,
-        const Res = 1 << 2,
-        const Pub = 1 << 3,
-        const Cmd = 1 << 4,
-        
+        // const None = 0, TODO: no flags needs a flag! and purpose
+        const Ping = 1, // may be removed in favor of a blank req
+        const Req = 1 << 1, // request
+        const Res = 1 << 2, // response
+        const Pub = 1 << 3, // publishing to an endpoint
+        const G1  = 1 << 4, // guaranteed at least once
+
+        // currently reserved bits for future extension
         const R1 = 1 << 5,
         const R2 = 1 << 6,
         const R3 = 1 << 7,
