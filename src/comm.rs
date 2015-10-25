9e3d2af8edeb66184336d12f0544dcd6fae80a48
viperscape <chrisfgill@gmail.com>
2015-10-25 14:33:35 -0400

    ping pong test

--- src/comm.rs
+++ src/comm.rs
@@ -13,25 +13,17 @@ pub fn listen(ip: Ipv4Addr, port: u16) {
     let src = SocketAddrV4::new(ip, port);
     if let Some(mut socket) = UdpSocket::bind(src).ok() {
         socket.set_read_timeout(Some(Duration::new(1,0)));
-        
-        let client = Client::blank();
-        ping_req(&client,src,&mut socket);
-
         let mut buf = [0; MAX_LEN];
         
-        {let (msg,src) = collect_msg(&mut buf, &mut socket);
-        let client = Client::from_msg(&msg);
-        
-        if Msg::auth(&client,&msg) {
-            println!("auth {:?}",msg.data);
-            handler(&client,&msg,src,&mut socket);
-        }
-        else { println!("not auth") }}
-
-        let (msg,src) = collect_msg(&mut buf, &mut socket);
-        let client = Client::from_msg(&msg);
-        if Msg::auth(&client,&msg) {
-            println!("auth {:?}",msg.data);
+        loop {
+            let (msg,src) = collect_msg(&mut buf, &mut socket);
+            let client = Client::from_msg(&msg);
+            
+            if Msg::auth(&client,&msg) {
+                println!("auth {:?} {:?}",msg.data, msg.flags());
+                handler(&client,&msg,src,&mut socket);
+            }
+            else { println!("not auth") }
         }
         
     }
@@ -39,10 +31,32 @@ pub fn listen(ip: Ipv4Addr, port: u16) {
 
 }
 
+pub fn send_ping(ip: Ipv4Addr, port: u16) {
+    let src = SocketAddrV4::new(ip, 55265);
+    let dest = SocketAddrV4::new(ip, port);
+    if let Some(mut socket) = UdpSocket::bind(src).ok() {
+        let client = Client::blank();
+        ping_req(&client,dest,&mut socket);
+
+        socket.set_read_timeout(Some(Duration::new(1,0)));
+        let mut buf = [0; MAX_LEN];
+        let (msg,src) = collect_msg(&mut buf, &mut socket);
+        let client = Client::from_msg(&msg);
+        if Msg::auth(&client,&msg) {
+            println!("auth {:?} {:?}",msg.data, msg.flags());
+        }
+        else { println!("not auth") }
+    }
+}
+
+
 pub fn reqres() {
+    use std::thread;
+    
     let ip = Ipv4Addr::new(127, 0, 0, 1);
     let port = 12345;
-    listen(ip,port);
+    let s = thread::spawn(move || { listen(ip,port) });
+    send_ping(ip,port);
 }
 
 /// command handler for flags
@@ -63,14 +77,16 @@ pub fn ping_req(client: &Client,
     let d = [0u8];
     let m = MsgBuilder::new(client,&d[..]).
         flag(flags::Ping).flag(flags::Req).build();
-    socket.send_to(&m.into_vec()[..],src);
+    let r = socket.send_to(&m.into_vec()[..],src);
+    println!("ping req: {:?}",r);
 }
 pub fn ping_res(client: &Client,
                 src: SocketAddr, socket: &mut UdpSocket) {
     let d = [0u8];
     let m = MsgBuilder::new(client,&d[..]).
         flag(flags::Ping).flag(flags::Res).build();
-    socket.send_to(&m.into_vec()[..],src);
+    let r = socket.send_to(&m.into_vec()[..],src);
+    println!("ping res: {:?}",r);
 }
 
 pub fn collect_msg<'d> (buf: &'d mut [u8;MAX_LEN], socket: &mut UdpSocket) -> (Msg<'d>,SocketAddr) {
